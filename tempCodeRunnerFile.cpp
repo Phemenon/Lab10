4612714e2f896e11b414477941a62c031b47548e
Phenomenon <xiii15499@gmail.com.com>
2024-12-17 23:07:39 +0700

    lab10_3

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,6 +1,34 @@
-cout<<"-------------------- BOOM ---------------------\n";
-	string textline;
-	while (getline(source,textline)){
-		cout << textline <<"\n";
-	}
-	cout<<"-------------------- HA!! ---------------------\n";
+#include<iostream>
+#include<fstream>
+#include<string>
+#include<cmath>
+#include <iomanip>
+
+using namespace std;
+
+int main(){
+    int count=0;
+    double mean = 0, svg=0, sum = 0, deviation = 0;
+    string textline;
+    ifstream source("score.txt");
+
+    while (getline(source,textline)){
+        mean += atof(textline.c_str());
+        count++;
+    }
+    svg = mean/count;
+
+    source.clear();
+    source.seekg(0, ios::beg);
+
+    while (getline(source,textline)){
+        double num = atof(textline.c_str());
+        sum += (num - mean) * (num - mean);        count++;
+    }
+    deviation = sqrt(sum);
+
+    cout << "Number of data = " << count << "\n";
+    cout << setprecision(3);
+    cout << "Mean = " << mean << "\n";
+    cout << "Standard deviation = " << deviation << "\n";
+}
